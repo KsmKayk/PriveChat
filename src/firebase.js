b5d92059d2ac724ce39c25baf704b508c58f5df9
Kayk Mascarenhas <kaykdsg@gmail.com>
2022-12-26 15:32:52 -0300

    firebase and env setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-require("dotenv").config();
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId,
+  apiKey: "AIzaSyBGXNvJb4c-K0E6RNpQfubWnFtBt02d2ek",
+  authDomain: "prive-chat-60268.firebaseapp.com",
+  projectId: "prive-chat-60268",
+  storageBucket: "prive-chat-60268.appspot.com",
+  messagingSenderId: "723233716260",
+  appId: "1:723233716260:web:af0ebf1e49dccdfb1840b1",
+  measurementId: "G-98PZHVJBWX",
 };
 
 export const app = initializeApp(firebaseConfig);
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import './styles.scss';
+
 import addAvatar from '../../img/addAvatar.png'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase';
 
 function Register() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user)
+  
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+  }
+
+  
+
   return (
     <div className='registerFormContainer'>
       <div className="registerFormWrapper">
         <span className="logo">Prive Chat</span>
         <span className="title">Register</span>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder='display name'/>
           <input type="email" placeholder='email'/>
           <input type="password" placeholder='password'/>
